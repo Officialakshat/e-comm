f214f85895cd391463000aebf31cc66d77e3659d
Officialakshat <165244500+Officialakshat@users.noreply.github.com>
2026-08-24 10:49:58 +0530

    update

--- server/src/middleware/authMiddleware.js
+++ server/src/middleware/authMiddleware.js
@@ -40,3 +40,14 @@ export const protect = async (req, res, next) => {
     });
   }
 };
+
+export const admin = (req, res, next) => {
+  if (req.user && req.user.role === "admin") {
+    next();
+  } else {
+    return res.status(403).json({
+      success: false,
+      message: "Not authorized as admin",
+    });
+  }
+};
